42076367b1264aa648e45917b5d9dc2d548ae21d
Luz <luz@infinixsoft.com>
2022-08-16 20:58:25 -0300

    Navigation

--- app/src/main/java/com/luz/melisearch/ui/SearchActivity.kt
+++ app/src/main/java/com/luz/melisearch/ui/SearchActivity.kt
@@ -7,19 +7,20 @@ import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.provider.SearchRecentSuggestions
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.SearchView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import com.luz.melisearch.R
 import com.luz.melisearch.databinding.ActivitySearchBinding
 import com.luz.melisearch.ui.base.BaseActivity
 import com.luz.melisearch.ui.search.OnClickSuggestionListener
 import com.luz.melisearch.ui.search.results.ResultsFragment
 import com.luz.melisearch.ui.search.suggestions.SuggestionsFragment
+import com.luz.melisearch.utils.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -29,19 +30,22 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class SearchActivity : BaseActivity(), OnClickSuggestionListener {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding : ActivitySearchBinding
+    private lateinit var binding: ActivitySearchBinding
 
-    private var suggestionsFragment : SuggestionsFragment? = null
+    private var suggestionsFragment: SuggestionsFragment? = null
+    private var searchView: SearchView? = null
+
+    private var lastQuery : String? = null
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_search, menu)
-        (menu.findItem(R.id.search).actionView as? SearchView)?.init()
+        searchView = (menu.findItem(R.id.search).actionView as? SearchView)
+        searchView?.init()
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.clearSuggestions){
+        if (item.itemId == R.id.clearSuggestions) {
             clearHistorySuggestions()
             return true
         }
@@ -57,15 +61,12 @@ class SearchActivity : BaseActivity(), OnClickSuggestionListener {
         setSupportActionBar(binding.toolbar)
         supportActionBar!!.title = ""
 
-        /*val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)*/
-
         handleIntent(intent)
 
         val navHostFragment: Fragment? =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_search)
-        suggestionsFragment = navHostFragment?.childFragmentManager?.fragments?.get(0) as? SuggestionsFragment
+        suggestionsFragment =
+            navHostFragment?.childFragmentManager?.fragments?.get(0) as? SuggestionsFragment
         suggestionsFragment?.setOnClickSuggestionListener(this)
     }
 
@@ -84,7 +85,7 @@ class SearchActivity : BaseActivity(), OnClickSuggestionListener {
         return contentResolver?.query(uri, null, selection, selArgs, null)
     }
 
-    private fun SearchView.init(){
+    private fun SearchView.init() {
         isIconifiedByDefault = false
 
         // Full width of the SearchView
@@ -94,34 +95,47 @@ class SearchActivity : BaseActivity(), OnClickSuggestionListener {
 
         setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                if (!query.isNullOrBlank()){
+                if (!query.isNullOrBlank()) {
                     saveQueryToHistory(query)
                     search(query)
+                    hideKeyboard()
                 }
                 return true
             }
 
             override fun onQueryTextChange(query: String?): Boolean {
-                if (!query.isNullOrBlank())
+                lastQuery = query
+                val navHost =
+                    supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_search)
+                if (navHost?.childFragmentManager?.fragments?.firstOrNull() != suggestionsFragment) {
+                    findNavController(R.id.nav_host_fragment_content_search)
+                        .navigate(R.id.action_to_SuggestionsFragment)
+                }
+                if (!query.isNullOrBlank()) {
                     getRecentSuggestions(query)?.let { showSuggestions(it) }
-                else
+                } else {
                     clearSuggestions()
+                }
                 return false
             }
         })
 
         setOnCloseListener { false }
+
+        lastQuery?.let {
+            setQuery(it, false)
+        }
     }
 
-    private fun showSuggestions(cursor : Cursor){
+    private fun showSuggestions(cursor: Cursor) {
         suggestionsFragment?.showSuggestions(cursor)
     }
 
-    private fun clearSuggestions(){
+    private fun clearSuggestions() {
         suggestionsFragment?.clearSuggestions()
     }
 
-    private fun saveQueryToHistory(query : String){
+    private fun saveQueryToHistory(query: String) {
         SearchRecentSuggestions(
             this,
             MySuggestionProvider.AUTHORITY,
@@ -151,14 +165,16 @@ class SearchActivity : BaseActivity(), OnClickSuggestionListener {
     }
 
     private fun search(query: String) {
+        lastQuery = query
         findNavController(R.id.nav_host_fragment_content_search).navigate(
-            R.id.action_to_ResultsFragment,
+            R.id.resultsFragment, //action //resultsFragment
             bundleOf(ResultsFragment.EXTRA_KEYWORD to query)
         )
     }
 
     // No need to save query to history
     override fun onClickSuggestion(suggestion: String) {
+        searchView?.setQuery(suggestion, false)
         search(suggestion)
     }
 
--- app/src/main/java/com/luz/melisearch/utils/AppUtils.kt
+++ app/src/main/java/com/luz/melisearch/utils/AppUtils.kt
@@ -1,8 +1,27 @@
 package com.luz.melisearch.utils
 
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
+
 /**
  * Created by Luz on 16/8/2022.
  */
 fun Float.toPriceFormat() : String{
     return String.format("$ %.2f", this)
+}
+
+fun Fragment.hideKeyboard() {
+    view?.let { activity?.hideKeyboard(it) }
+}
+
+fun Activity.hideKeyboard() {
+    hideKeyboard(currentFocus ?: View(this))
+}
+
+fun Context.hideKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
